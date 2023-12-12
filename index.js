7eda14328b41ce5ab4ede32dc63b5783ac1641b1
Muhammad Saikat <smsaikat000@gmail.com>
2023-12-12 10:52:00 +0600

    add route for get company data and crate jwt

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const port = process.env.PORT || 5000
-app.use(express.json())
-app.use(cors())
-app.get('/', (req, res) => {
-  res.send('Welcome to job portal')
-})
-
+const cors = require("cors");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Welcome to job portal");
+});
+const VerifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.JWT, (error, decode) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decode = decode;
+    next();
+  });
+};
 //  ! -------------- connect with mongodb-----------------
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lihpnz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +39,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,22 +48,38 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    const Users = client.db('Job-Portal').collection('Users')
-    const CompanyProfile = client.db('Job-Portal').collection('CompanyProfile')
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const Users = client.db("Job-Portal").collection("Users");
+    const CompanyProfile = client.db("Job-Portal").collection("CompanyProfile");
 
     // ! Post User Into db
-    app.post('/postUser',async(req,res)=>{
-      const data=await req.body
-      const result=await Users.insertOne(data)
-      res.send(result)
-    })
-    // ! Post  Company profile data 
-    app.post('/postCompanyData',async(req,res)=>{
-      const data=req.body 
-      const result =await CompanyProfile.insertOne(data)
-      res.send(result)
-    })
+    app.post("/postUser", async (req, res) => {
+      const data = await req.body;
+      const result = await Users.insertOne(data);
+      res.send(result);
+    });
+    // ! Post  Company profile data
+    app.post("/postCompanyData", async (req, res) => {
+      const data = req.body;
+      const result = await CompanyProfile.insertOne(data);
+      res.send(result);
+    });
+    // ! get company profile
+    app.get("/getCompany/:email",VerifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await CompanyProfile.findOne(query);
+      res.send(result);
+    });
+    // ! create jwt token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.JWT, { expiresIn: "1d" });
+      res.send({ token });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,10 +87,8 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("welcome");
+});
 
-app.get('/',async(req,res)=>{
-    
-    res.send('welcome')
-})
-
-app.listen(port)
+app.listen(port);
