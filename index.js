a7a66fa4917fc75a396a721fc63fd5e2c27f07a7
Muhammad Saikat <smsaikat000@gmail.com>
2023-12-14 21:50:12 +0600

    add a route to get a single route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -33,7 +34,6 @@ const VerifyJwt = (req, res, next) => {
 };
 
 //  ! -------------- connect with mongodb-----------------
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lihpnz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -106,6 +106,19 @@ async function run() {
     app.post('/markJob',async(req,res)=>{
       const data=req.body 
       const result=await markJob.insertOne(data) 
+      res.send(result)
+    })
+    // ! get candidate's mark job
+    app.get(`/markJob/:email`,VerifyJwt,async(req,res)=>{
+      const email=req.params.email 
+      const query={candidate:email}
+      const result=await markJob.find(query).toArray()
+      res.send(result)
+    })
+    app.get(`/getSingleJob/:id`,VerifyJwt,async(req,res)=>{
+      const id=req.params.id 
+      const query={_id:new ObjectId(id)}
+      const result=await Jobs.findOne(query)
       res.send(result)
     })
     // ! post Employer Profile
